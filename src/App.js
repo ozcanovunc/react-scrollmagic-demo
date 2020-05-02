561a75faaae23b763c5f0a4a3024a5df4357c230
ozcanovunc <ozcanovunc@outlook.com>
2020-05-02 22:54:26 +0300

    In progress

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import Resume from "./Components/Resume";
 import Testimonials from "./Components/Testimonials";
 import Portfolio from "./Components/Portfolio";
 
+// import ScrollMagic from 'scrollmagic';
+// import 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators';
+
+// import { Controller, Scene } from 'react-scrollmagic';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -23,6 +28,56 @@ class App extends Component {
 
   render() {
     let { core, resume, portfolio, testimonials } = this.state.resumeData;
+
+    // const intro = document.querySelector(".intro");
+    // const video = intro.querySelector("video");
+    // const text = intro.querySelector("h1");
+    
+    // const controller = new ScrollMagic.Controller();
+    // const scene = new ScrollMagic.Scene({
+    //   duration: 1000, // video suresi
+    //   triggerElement: intro,
+    //   triggerHook: 0
+    // })
+
+    // scene
+    // .addIndicators()
+    // .addTo(controller);
+
+
+    
+    return (
+ 
+      <div>
+        {/* <Controller>
+          <Scene 
+            duration={2000}
+            triggerElement=".intro"
+            indicators={true}
+            triggerHook={0}
+            pin
+            
+          >
+            <div className="intro">
+              <h1>Ozcan Ovunc</h1>
+              <video src="./videos/background.mp4"></video>
+            </div>
+          </Scene>
+        </Controller> */}
+        <div className="App">
+        <Header data={core} />
+        <About data={core} />
+        <Resume data={resume} />
+        <Portfolio data={portfolio} />
+        <Testimonials data={testimonials} />
+        <Footer data={core} />
+      </div>
+      </div>
+    );
+
+
+
+
     return (
       <div className="App">
         <Header data={core} />
