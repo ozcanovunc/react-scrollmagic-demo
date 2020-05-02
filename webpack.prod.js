275fb3e3ac8b5a792b0338c033222a57845e19c2
ozcanovunc <ozcanovunc@outlook.com>
2020-05-03 01:55:11 +0300

    build: v1.0.0

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,8 +5,8 @@ const path = require("path");
 module.exports = merge(common, {
   mode: "production",
   output: {
-    path: path.join(__dirname, 'build'),
-    publicPath: '/build',
+    path: path.join(__dirname),
+    publicPath: '/',
     filename: '[name].js'
   }
 });
